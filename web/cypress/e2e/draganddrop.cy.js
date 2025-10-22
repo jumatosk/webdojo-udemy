36dcaf4850cee99928ead0c6e6d670ec19798acf
Juliana Matos <jumatosk@gmail.com>
2025-10-22 14:25:14 -0300

    feat: interagindo com drang and drop

---
+++ web/cypress/e2e/draganddrop.cy.js
@@ -0,0 +1,17 @@
+describe("Kanban Board", () => {
+  it("Deve mover uma tarefa de ToDo para Done e atualizar o board.", () => {
+    cy.start();
+    cy.submitLogin(Cypress.env("USER_EMAIL"), Cypress.env("USER_PASSWORD"));
+    cy.contains("Kanban").click();
+
+    const dataTransfer = new DataTransfer();
+
+    cy.contains("div[draggable=true]", "Documentar API").trigger("dragstart", {
+      dataTransfer,
+    });
+
+    cy.get(".column-done")
+      .trigger("drop", { dataTransfer })
+      .and("include.text", "Documentar API");
+  });
+});
