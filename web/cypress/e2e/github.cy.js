d76da957c88b6b32dd7b919229c0e6a203ee9221
Juliana Matos <jumatosk@gmail.com>
2025-11-03 14:38:47 -0300

    feat: lida com acesso a links externos

--- web/cypress/e2e/github.cy.js
+++ web/cypress/e2e/github.cy.js
@@ -26,4 +26,46 @@ describe("Gerenciamento de perfis no Github", () => {
 
     cy.get("@trProfile").contains("Dev").should("be.visible");
   });
+
+  it.skip("Deve poder remover um perfil do github", () => {
+    const profile = {
+      name: "Juliana Matos",
+      username: "devjumatosk",
+      profile: "Dev",
+    };
+
+    cy.get("#name").type(profile.name);
+    cy.get("#username").type(profile.username);
+    cy.get("#profile").type(profile.profile);
+
+    cy.contains("button", "Adicionar Perfil").click();
+
+    cy.contains("table tbody tr", profile.username)
+      .should("be.visible")
+      .as("trProfile");
+
+    cy.get("@trProfile").find("button[title='Remover perfil']").click();
+
+    cy.contains("table tbody", profile.username).should("not.exist");
+  });
+
+  it("Deve validar o link do github", () => {
+    const profile = {
+      name: "Juliana Matos",
+      username: "jumatosk",
+      profile: "Dev",
+    };
+
+    cy.get("#name").type("Juliana Matos");
+    cy.get("#username").type("jumatosk");
+    cy.get("#profile").type("Dev");
+
+    cy.contains("button", "Adicionar Perfil").click();
+
+    cy.contains("table tbody tr", profile.username)
+      .should("be.visible")
+      .as("trProfile");
+
+    cy.get("@trProfile").find("a[title='Abrir perfil no GitHub']").click();
+  });
 });
