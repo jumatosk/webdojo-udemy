88de5434adfb461ad9892960d914e8de74f6d564
Juliana Matos <jumatosk@gmail.com>
2025-11-04 10:16:53 -0300

    feat: interceptando chamadas externas

--- web/cypress/e2e/cep.cy.js
+++ web/cypress/e2e/cep.cy.js
@@ -8,9 +8,20 @@ describe("CEP", () => {
   });
 
   it("Deve validar a consulta de CEP", () => {
+    cy.intercept("GET", `https://viacep.com.br/ws/${address.cep}/json/`, {
+      statusCode: 200,
+      body: {
+        logradouro: address.street,
+        bairro: address.neighborhood,
+        localidade: address.city,
+        uf: address.state,
+      },
+    }).as("getCep");
     // Fazer a modelagem da massa conforme a modelagem da aplicação
     cy.get("#cep").type(address.cep);
     cy.contains("button", "Buscar").click();
+
+    cy.wait("@getCep");
 
     cy.get("#street").should("have.value", address.street);
     cy.get("#neighborhood").should("have.value", address.neighborhood);
