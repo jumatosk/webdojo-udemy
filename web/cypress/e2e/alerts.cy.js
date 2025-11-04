ffe45de06f502bce5fcc0780cf7950528439039c
Juliana Matos <jumatosk@gmail.com>
2025-11-04 08:47:07 -0300

    feat: lidando com alert - botões Ok e Cancelar

---
+++ web/cypress/e2e/alerts.cy.js
@@ -0,0 +1,31 @@
+describe("Validações de Alertas em Javascript", () => {
+  beforeEach(() => {
+    cy.start();
+    cy.submitLogin(Cypress.env("USER_EMAIL"), Cypress.env("USER_PASSWORD"));
+    cy.goTo("Alertas JS", "JavaScript Alerts");
+  });
+
+  it("Deve validar a mensagem de alerta", () => {
+    cy.on("window:alert", (msg) => {
+      expect(msg).to.equal("Olá QA, eu sou uma Alert Box!");
+    });
+    cy.contains("button", "Mostrar Alert").click();
+  });
+
+  it("Deve confirmar um diálogo e validar a resposta positiva", () => {
+    cy.on("window:confirm", (msg) => {
+      expect(msg).to.equal("Aperta um botão!");
+      return true;
+    });
+
+    cy.contains("button", "Mostrar Confirm").click();
+  });
+  it.only("Deve cancelar um diálogo e validar a resposta negativa", () => {
+    cy.on("window:confirm", (msg) => {
+      expect(msg).to.equal("Aperta um botão!");
+      return false;
+    });
+
+    cy.contains("button", "Mostrar Confirm").click();
+  });
+});
